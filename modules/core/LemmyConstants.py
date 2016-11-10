bfbd89e5194bc7040bc9971e1b15b047069cf9d0
William Holt <wholt@odecee.com.au>
2016-11-10 15:45:37 +1100

    replace no_entry_sign error symbol with red X

--- modules/core/LemmyConstants.py
+++ modules/core/LemmyConstants.py
@@ -3,6 +3,6 @@ from ddict import ddict
 class LemmyConstants:
 	def __init__(self):
 		self.error = ddict()
-		self.error.symbol = ":no_entry_sign:"
+		self.error.symbol = ":x:"
 		self.error.notMod = "User must be moderator or above to perform this action."
 		self.error.notAdmin = "User must be administrator to perform this action."
