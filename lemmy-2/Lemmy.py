5331f455eb6b5b4875590b44ab7e2f5a3f9057cc
Will Holt <halbrd@gmail.com>
2017-09-14 23:33:01 +1000

    add temporary shutdown function to lemmy-2

--- lemmy-2/Lemmy.py
+++ lemmy-2/Lemmy.py
@@ -13,18 +13,20 @@ class NoConfigException(Exception):
 
 class Lemmy:
 	def log(self, message):
-		print('[{}] {}'.format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), message))
+		output = '[{}] {}'.format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), message)
+
+		print(output)
 
 		if self.config['log_file']:
 			with open(self.config['log_file'], 'a') as f:
-				f.write(message + '\n')
+				f.write(output + '\n')
 
 	def __init__(self, token):
 		self.client = discord.Client()
 
 		# import config
 		if not os.path.isfile('config.json'):
-			raise NoConfigException()
+			raise NoConfigException
 
 		self.config = json.load(open('config.json', 'r'))
 
@@ -32,6 +34,9 @@ class Lemmy:
 		@self.client.event
 		async def on_message(message):
 			self.log(f'({message.channel.server.name}) {message.author.name} => #{message.channel.name}: {message.content}')
+
+			if message.content == '$shutdown' and message.author.id == '77041679726551040':
+				await self.client.logout()
 
 		@self.client.event
 		async def on_ready():
@@ -43,5 +48,5 @@ class Lemmy:
 
 if __name__ == '__main__':
 	if not os.path.isfile('config.json'):
-		raise NoConfigException()
+		raise NoConfigException
 	lemmy = Lemmy(json.load(open('config.json', 'r'))['token'])
