faa829e7eaf9d1939117a6d658ff912679c98cc8
Will Holt <halbrd@gmail.com>
2017-10-22 14:48:19 +1100

    refactor function exit codes to exceptions

--- lemmy-2/modules/module.py
+++ lemmy-2/modules/module.py
@@ -1,6 +1,19 @@
 class Module:
-    def __init__(self, client):
-        self.client = client
+    class CommandError(Exception):
+        def __init__(self, message=None):
+            if message is None:
+                usage = getattr(self, f'cmd_{terms[0]}_usage')
+                message = 'Usage:\n' + '\n'.join(['`' + form + '`' for form in usage])
+
+            super(CommandError, self).__init__(message)
+
+    class CommandSuccess(Exception):
+        def __init__(self, message=None):   # is this superfluous?
+            super(CommandSuccess, self).__init__(message)
+
+    def __init__(self, lemmy):
+        self.lemmy = lemmy
+        self.client = lemmy.client   # this line is arguably bad taste code, but the name binding removes a *lot* of typing
         self.commands = { function[4:]: getattr(self, function) for function in dir(self) if function.startswith('cmd_') }
 
     async def on_message(self, message):
@@ -9,31 +22,35 @@ class Module:
     async def call_functions(self, message):
         terms = Module.deconstruct_message(message)
 
-        if len(terms) > 0 and terms[0] in self.commands:
-            exit_code = await self.commands[terms[0]](message, terms[1:])
+        try:
+            symbol = self.lemmy.config_try_key("server_config", message.channel.server.id, "symbol")
+        except ( AttributeError, KeyError ):
+            symbol = self.lemmy.config["default_symbol"]
 
-            # I was really torn over using exit codes or exceptions for this
-            # I went with exit codes because otherwise I'd be raising an exception for send_success, which isn't an error condition
-            # Python's design paradigms are flexible and I could use an exception for non-error purposes, but it feels dirty and wrong
-            # ...somehow less so than string codes...
-            if exit_code is None:
-                pass
-            elif exit_code == 'usage':
-                await self.send_error(message, getattr(self, f'cmd_{terms[0]}_usage'))
-            elif exit_code == 'success':
-                await self.send_success(message)
-            else:
-                raise ValueError(f'"{exit_code}" is not a valid exit code')
+        if len(terms) > 0 and terms[0].startswith(symbol):
+            terms[0] = terms[0][len(symbol):]
 
+            if terms[0] in self.commands:
+                try:
+                    await self.commands[terms[0]](message, terms[1:])
+                except CommandError as e:
+                    await self.send_error(message, e.message)
+                except CommandSuccess as e:
+                    await self.send_success(message, e.message)
 
     @staticmethod
     def deconstruct_message(message):
-        # this function could be much more complex, but it doesn't need to be more heavily engineered than this
+        # TODO: preserve quoted sections
         return message.content.split()
 
-    async def send_error(self, message, usage):
+    async def send_error(self, message, comment=None):
         await self.client.add_reaction(message, '❌')
-        await self.client.send_message(message.channel, 'Usage:\n' + '\n'.join(['`' + form + '`' for form in usage]))
 
-    async def send_success(self, message):
+        if comment:
+            await self.client.send_message(message.channel, comment)
+
+    async def send_success(self, message, comment=None):
         await self.client.add_reaction(message, '✅')
+
+        if comment:
+            await self.client.send_message(message.channel, comment)
--- lemmy-2/modules/tests.py
+++ lemmy-2/modules/tests.py
@@ -3,10 +3,14 @@ from module import Module
 class Tests(Module):
     info = 'Tests features of lemmy-2'
 
-    cmd_send_error_usage = [ 'example 1', 'example 2' ]
+    cmd_send_error_usage = [ 'send_error', 'send_error <message>' ]
     async def cmd_send_error(self, message, args):
-        return 'usage'
+        raise Module.CommandError(args[0] if args else None)
 
     cmd_send_success_usage = [ 'example' ]
     async def cmd_send_success(self, message, args):
-        return 'success'
+        raise Module.CommandSuccess()
+
+    cmd_channel_type_usage = [ 'channel_type' ]
+    async def cmd_channel_type(self, message, args):
+        await self.client.send_message(message.channel, type(message.channel))
