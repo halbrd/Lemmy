cc6076c5d56d5a348dfe275bdc602f00b1f44d15
Will Holt <halbrd@gmail.com>
2017-12-27 21:34:03 +1100

    add SpookyGhost module

---
+++ lemmy-2/modules/spookyghost.py
@@ -0,0 +1,6 @@
+from module import Module
+
+class SpookyGhost(Module):
+	async def on_message(self, message):
+		if str(message.author.status) == 'offline':
+			await self.client.add_reaction(message, '👻')
