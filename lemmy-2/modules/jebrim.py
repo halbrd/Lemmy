ff6503059b30c67860197d2eb1b2186e9bf51d6e
fastzr <marcel_hoh@outlook.com>
2018-02-07 01:47:03 +1100

    tested jebrim

--- lemmy-2/modules/jebrim.py
+++ lemmy-2/modules/jebrim.py
@@ -3,19 +3,17 @@ import json
 import random
 
 class jebrim(Module):
+	jsonKey = 'jebrimLinks'
+	jsonLocation = 'modules/jebrimTweets.json'
 
 	docs = {
 		'description': 'Posts jebrim tweets'
 	}
 
-	tweetList = [];
-	jsonKey = 'jebrimLinks'
-	jsonLocation = 'jebrimTweets.json'
-
 	def __init__(self, client):
 		Module.__init__(self, client)
-		jsonData = json.load(open(jsonLocation,r))
-		tweetList = jsonData[jsonKey]
+		jsonData = json.load(open(jebrim.jsonLocation,'r'))
+		self.tweetList = jsonData[jebrim.jsonKey]
 
 
 	docs_addjebrim = {
@@ -23,13 +21,21 @@ class jebrim(Module):
 	}
 
 	async def cmd_addjebrim(self, message, args, kwargs):
-		tweetList.append(args[1])
-		jsonData = {jsonKey : jsonLocation}
-		json.dump(jsonData, open(jsonLocation , 'w'))
+		if args[0] in self.tweetList:
+			await self.send_error(message)
+		else:
+			if not args[0].startswith("http"):
+				await self.send_error(message)
+				await self.client.send_message(message.channel, "Please send valid link")
+			else:
+				self.tweetList.append(args[0])
+				await self.send_success(message)
+				jsonData = {jebrim.jsonKey : self.tweetList}
+				json.dump(jsonData, open(jebrim.jsonLocation , 'w'))
 
 	docs_jebrim = {
 		'description': 'Posts jebrim tweet'
 	}
 
 	async def cmd_jebrim(self, message, args, kwargs):
-		await self.client.send_message(message.channel, random.choice(tweetList))
+		await self.client.send_message(message.channel, random.choice(self.tweetList))
