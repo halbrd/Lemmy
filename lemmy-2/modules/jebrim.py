6b0f48c87a2140147fddb483d3072b7122601ef9
Will Holt <halbrd@gmail.com>
2018-02-08 23:30:20 +1100

    tweak @marcelhoh's jebrim module

--- lemmy-2/modules/jebrim.py
+++ lemmy-2/modules/jebrim.py
@@ -4,36 +4,36 @@ import random
 import re
 
 class Jebrim(Module):
-	json_key = 'jebrim_links'
 	json_location = 'config/jebrim/tweets.json'
 
 	docs = {
-		'description': 'Posts random Jebrim tweets'
+		'description': 'Posts random Jebrim quotes'
 	}
 	def __init__(self, client):
 		Module.__init__(self, client)
 		json_data = json.load(open(Jebrim.json_location, 'r'))
-		self.tweet_list = json_data[Jebrim.json_key]
+		self.tweet_list = json_data['links']
 
 	docs_addjebrim = {
-		'description': 'Add link to Jebrim tweet to the list'
+		'description': 'Add a link to a Jebrim screenshot to the list',
+		'usage': 'addjebrim link'
 	}
 	async def cmd_addjebrim(self, message, args, kwargs):
 		if args[0] in self.tweet_list:
 			await self.send_error(message)
 			return
 
-		if not re.match('https?://', args[0]):
-			await self.send_error(message, comment='Please send valid link')
+		if not re.match('https?://.+\.(png|jpg|jpeg|gif)', args[0]):
+			await self.send_error(message)
 			return
 
 		self.tweet_list.append(args[0])
-		json_data = {Jebrim.json_key : self.tweet_list}
+		json_data = { 'links' : self.tweet_list }
 		json.dump(json_data, open(Jebrim.json_location, 'w'), indent=4)
 		await self.send_success(message)
 
 	docs_jebrim = {
-		'description': 'Posts random Jebrim tweet'
+		'description': 'Posts a random Jebrim quote'
 	}
 	async def cmd_jebrim(self, message, args, kwargs):
 		await self.client.send_message(message.channel, random.choice(self.tweet_list))
