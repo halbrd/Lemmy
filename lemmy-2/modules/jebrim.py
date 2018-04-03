9ffae02604053f830b8854b0beaf3babff82c974
Will Holt <halbrd@gmail.com>
2018-04-03 23:26:50 +1000

    tweak Jebrim module to better adhere to standards

--- lemmy-2/modules/jebrim.py
+++ lemmy-2/modules/jebrim.py
@@ -12,17 +12,17 @@ class Jebrim(Module):
 
 		self.tweets = self.load_data('tweets')['links']
 
-	docs_add_jebrim = {
-		'description': 'Add a new Jebrim screenshot to the list (Imgur direct links only)',
-		'usage': 'add_jebrim link'
+	docs_jebrim_add = {
+		'description': 'Adds a new Jebrim screenshot to the list',
+		'usage': 'jebrim_add link'
 	}
-	async def cmd_add_jebrim(self, message, args, kwargs):
+	async def cmd_jebrim_add(self, message, args, kwargs):
 		if args[0] in self.tweets:
-			await self.send_error(message)
+			await self.send_error(message, comment='link already exists in database')
 			return
 
 		if not re.match('https://i\.imgur\.com/[A-Za-z0-9]+\.(png|jpg)/?', args[0]):
-			await self.send_error(message)
+			await self.send_error(message, comment='link must be an Imgur direct link')
 			return
 
 		self.tweets.append(args[0])
