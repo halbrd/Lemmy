c5415665a5e18426cdefb838c26c1485b82ff743
William Holt <halbrd@gmail.com>
2018-04-30 19:06:56 +1000

    move module.py to project root

--- lemmy-2/modules/_example.py
+++ lemmy-2/modules/_example.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('..')
 from module import Module
 
 class Example(Module):
--- lemmy-2/modules/customcommands.py
+++ lemmy-2/modules/customcommands.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('..')
 from module import Module
 
 from itertools import zip_longest
