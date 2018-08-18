03923219943dc965b096c2b78642172300826d22
Will Holt <halbrd@gmail.com>
2018-08-18 13:31:21 +1000

    simplify storage and saving/retrieval of Jebrim tweets

--- lemmy-2/modules/jebrim.py
+++ lemmy-2/modules/jebrim.py
@@ -11,10 +11,9 @@ class Jebrim(Module):
 	docs = {
 		'description': 'Posts random Jebrim quotes'
 	}
-	def __init__(self, client):
-		Module.__init__(self, client)
 
-		self.tweets = self.load_data('tweets', static=True)['links']
+	def get_tweets(self):
+		return self.load_data('tweets', static=True)
 
 	docs_jebrim_add = {
 		'description': 'Adds a new Jebrim screenshot to the list',
@@ -25,24 +24,26 @@ class Jebrim(Module):
 			await self.send_error(message)
 			return
 
-		if args[0] in self.tweets:
-			await self.send_error(message, comment='link already exists in database')
-			return
-
 		if not re.match('https://i\.imgur\.com/[A-Za-z0-9]+\.(png|jpg)/?', args[0]):
 			await self.send_error(message, comment='link must be an Imgur direct link')
 			return
 
-		self.tweets.append(args[0])
-		data = { 'links' : self.tweets }
-		self.save_data('tweets', data, static=True)
+		tweets = self.get_tweets()
+
+		if args[0] in tweets:
+			await self.send_error(message, comment='link already exists in database')
+			return
+
+		tweets.append(args[0])
+		self.save_data('tweets', tweets, static=True)
 		await self.send_success(message)
 
 	docs_jebrim = {
 		'description': 'Posts a random Jebrim quote'
 	}
 	async def cmd_jebrim(self, message, args, kwargs):
-		await self.client.send_message(message.channel, random.choice(self.tweets))
+		tweets = self.get_tweets()
+		await self.client.send_message(message.channel, random.choice(tweets))
 
 	docs_is_jebrim_suspended = {
 		'description': 'Checks if Jebrim is suspended on Twitter'
