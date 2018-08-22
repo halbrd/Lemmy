5a2681295dea26a3e89634b4cffe14d58a7eb6e4
William Holt <william.holt@digio.com.au>
2018-08-22 13:24:09 +1000

    style/formatting changes

--- lemmy-2/modules/emotes.py
+++ lemmy-2/modules/emotes.py
@@ -2,41 +2,33 @@ import sys
 sys.path.append('..')
 from module import Module
 
-from discord import File
-from os import listdir
-from os.path import isfile, join
+import discord
 
 WEBHOOK_NAME = 'WEBHOOK_BOT'
 EMOTE_PATH = '../pics/emotes'
 
 class Emotes(Module):
     docs = {
-            'description': 'Handles emotes'
-            }
+        'description': 'Handles emotes'
+    }
 
     def __init__(self, client):
         self.media = {}
-        for f in (f for f in listdir(EMOTE_PATH) if isfile(join(EMOTE_PATH, f))):
+        for f in (f for f in os.listdir(EMOTE_PATH) if os.path.isfile(os.path.join(EMOTE_PATH, f))):
             emote, ext = f.split('.')
             self.media[emote] = f
 
-    async def get_or_create_webhook(self, channel):
-        webhooks = await channel.webhooks()
-        if WEBHOOK_NAME in webhooks:
-            return webhooks[WEBHOOK_NAME]
-        else:
-            webhook = await channel.create_webhook(name=WEBHOOK_NAME)
-            return webhook
+    async def get_webhook(self, channel):
+        if not WEBHOOK_NAME in await channel.webhooks():
+            await channel.create_webhook(name=WEBHOOK_NAME)
+
+        return await channel.webhooks()[WEBHOOK_NAME]
 
     async def on_message(self, message):
         if message.content in self.media:
-            webhook = await self.get_or_create_webhook(message.channel)
-
-            display_name = message.author.display_name
-            avatar_url = message.author.avatar_url
-            imagePath = self.media[message.content]
+            webhook = await self.get_webhook(message.channel)
 
             with open(f'{EMOTE_PATH}/{imagePath}', 'rb') as image:
-                file = File(fp=image, filename=imagePath)
-                await webhook.send(username=display_name, avatar_url=avatar_url, file=file)
+                file = discord.File(fp=image, filename=self.media[message.content])
+                await webhook.send(username=message.author.display_name, avatar_url=message.author.avatar_url, file=file)
                 await message.delete()
