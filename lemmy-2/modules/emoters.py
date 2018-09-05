a81176e2cb8503f76594c885838562ad37e217fc
William Holt <william.holt@digio.com.au>
2018-09-05 11:52:56 +1000

    re-enable instance/static emote system

--- lemmy-2/modules/emoters.py
+++ lemmy-2/modules/emoters.py
@@ -30,10 +30,14 @@ class Emoters(Module):
         for arg in args:
             discord_file = None
 
-            if arg in self.emotes:
-                discord_file = self.get_image_as_discord_file(self.emotes[arg], 'emote')
-            elif arg in self.stickers:
-                discord_file = self.get_image_as_discord_file(self.stickers[arg], 'sticker')
+            if arg in self.instance_emotes:
+                discord_file = self.get_image_as_discord_file(self.instance_emotes[arg], 'emote', static=False)
+            elif arg in self.instance_stickers:
+                discord_file = self.get_image_as_discord_file(self.instance_stickers[arg], 'sticker', static=False)
+            elif arg in self.static_emotes:
+                discord_file = self.get_image_as_discord_file(self.static_emotes[arg], 'emote', static=True)
+            elif arg in self.static_stickers:
+                discord_file = self.get_image_as_discord_file(self.static_stickers[arg], 'sticker', static=True)
 
             if discord_file:
                 await self.send_image(discord_file, message.channel, vanity_username=message.author.name,
@@ -43,9 +47,9 @@ class Emoters(Module):
         if delete_message:
             await message.delete()
 
-    def _load_emoters(self, emoter_type):
+    def _load_emoters(self, emoter_type, static):
         # list all files in emoter directory
-        files = self.list_files(emoter_type, static=True)
+        files = self.list_files(emoter_type, static=static)
 
         # filter out files that can't be an emoter
         is_valid_emoter = lambda file_name: file_name.endswith('.gif') or file_name.endswith('.png')
@@ -58,14 +62,16 @@ class Emoters(Module):
         }
 
     def load_emotes(self):
-        self.emotes = self._load_emoters('emote')
+        self.static_emotes = self._load_emoters('emote', static=True)
+        self.instance_emotes = self._load_emoters('emote', static=False)
 
     def load_stickers(self):
-        self.stickers = self._load_emoters('sticker')
+        self.static_stickers = self._load_emoters('sticker', static=True)
+        self.instance_stickers = self._load_emoters('sticker', static=False)
 
     # TODO: refactor into filename -> bytes, bytes -> discordfile
-    def get_image_as_discord_file(self, file_name, emoter_type):
-        image_bytes = self.load_image(f'{emoter_type}/{file_name}', static=True)
+    def get_image_as_discord_file(self, file_name, emoter_type, static):
+        image_bytes = self.load_image(f'{emoter_type}/{file_name}', static=static)
         image_file = io.BytesIO(image_bytes)
         discord_file = discord.File(fp=image_file, filename=file_name)
         return discord_file
@@ -78,8 +84,10 @@ class Emoters(Module):
         await webhook.send(username=vanity_username, avatar_url=vanity_avatar_url, file=discord_file)
 
     async def cmd_emoter_list(self, message, args, kwargs):
-        await message.channel.send('Emotes: ' + str(self.emotes))
-        await message.channel.send('Stickers: ' + str(self.stickers))
+        await message.channel.send('Static emotes: ' + str(self.static_emotes))
+        await message.channel.send('Instance emotes: ' + str(self.instance_emotes))
+        await message.channel.send('Static stickers: ' + str(self.static_stickers))
+        await message.channel.send('Instance stickers: ' + str(self.instance_stickers))
 
     def normalize_png_pillow(self, png_bytes, target_size):
         # load bytes into PIL Image
