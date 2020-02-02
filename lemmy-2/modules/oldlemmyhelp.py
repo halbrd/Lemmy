3ed6aedd09aa3fe430cd7ab35f651e9f3c183cf1
William Holt <halbrd@gmail.com>
2020-02-02 14:15:38 +1100

    delete most of the remaining stuff in OldLemmyHelp

--- lemmy-2/modules/oldlemmyhelp.py
+++ lemmy-2/modules/oldlemmyhelp.py
@@ -8,25 +8,6 @@ class OldLemmyHelp(Module):
 		'description': 'Provides help documentation for Old Lemmy commands'
 	}
 
-	docs_refresh = {
-		'description': 'Checks for new emotes and stickers'
-	}
-	async def cmd_refresh(self, message, args, kwargs):
-		pass
-
-	docs_gifr = {
-		'description': 'Returns a random gif according to provided search terms',
-		'usage': 'gifr <search term>'
-	}
-	async def cmd_gifr(self, message, args, kwargs):
-		pass
-
-	docs_hero = {
-		'description': 'Returns a random Overwatch hero'
-	}
-	async def cmd_hero(self, message, args, kwargs):
-		pass
-
 	docs_lemmycoin = {
 		'description': 'Performs LemmyCoin-related operations',
 		'usage': 'lemmycoin -balance <username> -pay <username> <amount>',
@@ -35,39 +16,9 @@ class OldLemmyHelp(Module):
 	async def cmd_lemmycoin(self, message, args, kwargs):
 		pass
 
-	docs_lol = {
-		'description': 'You can look up items or some shit like that',
-		'usage': 'lol -item <item name>'
-	}
-	async def cmd_lol(self, message, args, kwargs):
-		pass
-
 	docs_restart = {
 		'description': 'Restarts Lemmy',
 		'admin_only': True
 	}
 	async def cmd_restart(self, message, args, kwargs):
 		pass
-
-	docs_skypeemotes = {
-		'description': 'Returns a list of available Skype emotes'
-	}
-	async def cmd_skypeemotes(self, message, args, kwargs):
-		pass
-
-	docs_tilt = {
-		'description': 'Tilts an emote',
-		'usage': 'tilt emote <amount>',
-		'examples': [ 'tilt KappaG', 'tilt Jebaited 180' ]
-	}
-	async def cmd_tilt(self, message, args, kwargs):
-		pass
-
-	docs_fusion = {
-		'description': 'Fuses Pokemon together',
-		'usage': 'fusion <first Pokemon> <second Pokemon>',
-		'examples': [ 'fusion shellder voltorb' ]
-	}
-	async def cmd_fusion(self, message, args, kwargs):
-		pass
-
