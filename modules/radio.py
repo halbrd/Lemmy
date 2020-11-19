1d1c703f5da8028dafc58694647251b7a91ea70b
William Holt <halbrd@gmail.com>
2020-11-19 14:03:12 +1100

    improve code layout

--- modules/radio.py
+++ modules/radio.py
@@ -7,35 +7,40 @@ class Radio(Module):
         'description': 'Plays audio in voice channels'
     }
 
-    def __init__(self, lemmy):
-        Module.__init__(self, lemmy)
+    def get_voice_clients(self):
+        return {
+            voice_client.guild.id: voice_client
+            for voice_client in self.client.voice_clients
+        }
 
-        # this stores all of the active voice clients
-        # it is indexed by server id because each server may only have one voice client
-        # [voice_channel.server.id] -> voice_client
-        # you can always call voice_client.channel and voice_client.guild
-        self.voice_clients = {}
+    async def cmd_lsvoice(self, message, args, kwargs):
+        await message.channel.send(self.get_voice_clients())
 
     docs_radio_on = {
         # TODO
     }
     async def cmd_radio_on(self, message, args, kwargs):
-        if message.guild.id in self.voice_clients:
-            raise Exception('already in a voice channel in this server')
-
         if message.author.voice is None or message.author.voice.channel is None:
-            raise Exception('invoker not in a voice channel')
+            await self.send_error(message, 'invoker not in a voice channel')
+            return
 
-        voice_client = await message.author.voice.channel.connect(timeout=10, reconnect=False)
-        self.voice_clients[voice_client.guild.id] = voice_client
+        voice_clients = self.get_voice_clients()
+
+        if message.guild.id in voice_clients:
+            await self.send_error(message, 'already in a voice channel in this server')
+            return
+
+        await message.author.voice.channel.connect(timeout=10, reconnect=False)
 
 
     docs_radio_off = {
         # TODO
     }
     async def cmd_radio_off(self, message, args, kwargs):
-        if not message.channel.guild.id in self.voice_clients:
-            raise Exception('not in a voice channel on this server')
+        voice_clients = self.get_voice_clients()
 
-        await self.voice_clients[message.channel.guild.id].disconnect()
-        del self.voice_clients[message.channel.guild.id]
+        if not message.guild.id in voice_clients:
+            await self.send_error(message, 'not in a voice channel on this server')
+            return
+
+        await voice_clients[message.guild.id].disconnect()
