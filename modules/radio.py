c9b9145defb7f445088c1b6cb1b99da66d28deb5
William Holt <halbrd@gmail.com>
2020-11-20 14:31:01 +1100

    implement youtube metadata retrieval

--- modules/radio.py
+++ modules/radio.py
@@ -1,11 +1,24 @@
+# hmmm
+# step 1: extract metadata from individual videos and playlists, given a link to either one
+# step 2: cache those videos to disk
+# step 3: move Lemmy around through the voice channels comfortably and effectively
+# step 4: all automated cleanup
+# step 5: queue and play audio
+
+
 import sys
 sys.path.append('..')
 from module import Module
+
+import discord
+import youtube_dl
 
 class Radio(Module):
     docs = {
         'description': 'Plays audio in voice channels'
     }
+
+    # general management functions
 
     def get_voice_clients(self):
         return {
@@ -13,8 +26,36 @@ class Radio(Module):
             for voice_client in self.client.voice_clients
         }
 
-    async def cmd_lsvoice(self, message, args, kwargs):
-        await message.channel.send(self.get_voice_clients())
+    # Youtube functions
+
+    def _extract_video_metadata(self, ytdl_output):
+        return {
+            'id': ytdl_output['id'],
+            'title': ytdl_output['title'],
+            'source': ytdl_output['webpage_url'],
+        }
+
+    def get_all_videos_metadata(self, url):
+        with youtube_dl.YoutubeDL() as ytdl:
+            result = ytdl.extract_info(
+                url,
+                download=False
+            )
+
+        if result.get('_type') == 'playlist':
+            # it's a playlist
+            videos = result['entries']
+        else:
+            # it's an individual video
+            videos = [result]
+
+        return [ self._extract_video_metadata(video) for video in videos ]
+
+    async def cmd_getmeta(self, message, args, kwargs):
+        import json
+        for url in args:
+            data = self.get_all_videos_metadata(url)
+            await message.channel.send(f'```\n{json.dumps(data, indent=2)}\n```')
 
     docs_radio_on = {
         # TODO
