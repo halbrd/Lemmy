d2dc70d0831d98b5ad5af70a8b11fc3f95be9e25
William Holt <halbrd@gmail.com>
2021-08-01 18:16:38 +1000

    lower default volume

--- modules/radio.py
+++ modules/radio.py
@@ -32,7 +32,7 @@ ffmpeg_options = {
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
+    def __init__(self, source, *, data, volume=0.25):
         super().__init__(source, volume)
 
         self.data = data
