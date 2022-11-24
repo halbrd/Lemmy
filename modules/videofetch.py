a28071d73498d7d067b9397ee46267dd678bed90
William Holt <git@halbrd.com>
2022-11-24 14:11:29 +1100

    add support for reddit videos on VideoFetch

--- modules/videofetch.py
+++ modules/videofetch.py
@@ -16,17 +16,29 @@ import re
 CACHE_LOC = 'cache/VideoFetch/'
 ATTEMPTS = 3
 
+DOWNLOAD_PATTERNS = [
+    re.compile('https?://\w+.tiktok.com/[^ ]+'),
+
+    re.compile('https?://\w+.reddit.com/r/\w+/comments/\w+.*'),
+    re.compile('https?://v.redd.it/\w+'),
+    re.compile('https?://redd.it/\w+'),
+]
+
 def clean_filename(filename):
     for c in ['<', '>', ':', '"', '/', '\\', '|', '?', '*']:
         filename = filename.replace(c, '')
     return filename
 
-async def send_tiktok(url, channel):
-    filename = CACHE_LOC + clean_filename(url) + '.mp4'
+async def download_and_send(url, channel):
+    base_filename = clean_filename(url)
+    base_file_loc = CACHE_LOC + base_filename
+
+    expected_filename = base_filename + '.mp4'
+    expected_file_loc = CACHE_LOC + base_filename + '.mp4'
 
     success = False
     for i in range(ATTEMPTS):
-        r = subprocess.run(['yt-dlp', '-S', 'vcodec:h264', '-o', filename, url])
+        r = subprocess.run(['yt-dlp', '-S', 'vcodec:h264', '-o', base_file_loc + '.%(ext)s', url])
         if r.returncode == 0:
             success = True
             break
@@ -34,10 +46,20 @@ async def send_tiktok(url, channel):
     if not success:
         return
 
-    with open(filename, 'rb') as f:
-      await channel.send(file=discord.File(f))
+    matching_files = [
+        f for f in os.listdir(CACHE_LOC)
+        if f.startswith(base_filename)
+    ]
 
-    os.remove(filename)
+    if expected_filename in matching_files:
+        try:
+            with open(expected_file_loc, 'rb') as f:
+                await channel.send(file=discord.File(f))
+        except:
+            pass
+
+    for f in matching_files:
+        os.remove(CACHE_LOC + f)
 
 
 class VideoFetch(Module):
@@ -50,5 +72,6 @@ class VideoFetch(Module):
             # just in case
             return
 
-        for match in re.findall('https?://\w+.tiktok.com/[^ ]+', message.content):
-            await send_tiktok(match, message.channel)
+        for pattern in DOWNLOAD_PATTERNS:
+            for match in re.findall(pattern, message.content):
+                await download_and_send(match, message.channel)
