c9c45ace2c547301d725d089aa999345e121601c
William Holt <git@halbrd.com>
2023-04-21 11:59:34 +1000

    require discord links

--- modules/jebrim.py
+++ modules/jebrim.py
@@ -25,8 +25,8 @@ class Jebrim(Module):
             await self.send_error(message)
             return
 
-        if not re.match('https://i\.imgur\.com/[A-Za-z0-9]+\.(png|jpg)/?', args[0]):
-            await self.send_error(message, comment='link must be an Imgur direct link')
+        if not re.match('https://cdn\.discordapp\.com/attachments/[0-9]+/[0-9]+/.+\.(png|jpg)/?', args[0]):
+            await self.send_error(message, comment='link must be a Discord direct link')
             return
 
         tweets = self.get_tweets()
