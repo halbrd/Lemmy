ca10b9d1b7bc483cd7ceb73de0265e8bcd035e4a
William Holt <git@halbrd.com>
2025-04-26 18:43:24 +1000

    remove cookies from radio

--- modules/radio.py
+++ modules/radio.py
@@ -12,7 +12,6 @@ CACHE_LOC = 'cache/Radio/'
 
 # copied from https://github.com/Rapptz/discord.py/blob/master/examples/basic_voice.py
 ytdl_format_options = {
-    'cookiefile': 'data/Radio/cookies.txt',
     'format': 'bestaudio/best',
     'outtmpl': CACHE_LOC + '%(extractor)s-%(id)s-%(title)s.%(ext)s',
     'restrictfilenames': True,
